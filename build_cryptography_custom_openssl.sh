3c61eed2891de1372641c2ea297cc47482360b78
hanthesecond <81246536+hanthesecond@users.noreply.github.com>
2021-04-12 15:45:36 -0500

    Update build_cryptography_custom_openssl.sh

--- build_cryptography_custom_openssl.sh
+++ build_cryptography_custom_openssl.sh
@@ -8,7 +8,7 @@ pip install -U wheel pip
 curl -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
 tar xvf openssl-${OPENSSL_VERSION}.tar.gz
 cd openssl-${OPENSSL_VERSION}
-./config no-shared no-ssl2 -fPIC --prefix=${CWD}/openssl
+./config no-shared no-ssl2 no-ssl3 -fPIC --prefix=${CWD}/openssl
 make && make install
 cd ..
-CFLAGS="-I${CWD}/openssl/include" LDFLAGS="-L${CWD}/openssl/lib" pip wheel --no-binary cryptography
+CFLAGS="-I${CWD}/openssl/include" LDFLAGS="-L${CWD}/openssl/lib" pip wheel --no-binary :all: cryptography
